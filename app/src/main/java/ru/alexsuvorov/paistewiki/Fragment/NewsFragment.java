f1cff983a8990eb380b7eaa0c5fa3ee7309bd16e
AlexSuvorov <alexsuvorov2k@gmail.com>
2018-07-19 16:44:34 +0400

    -c

--- app/src/main/java/ru/alexsuvorov/paistewiki/Fragment/NewsFragment.java
+++ app/src/main/java/ru/alexsuvorov/paistewiki/Fragment/NewsFragment.java
@@ -4,58 +4,29 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.TableLayout;
-import android.widget.TextView;
-
-import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
 
 import ru.alexsuvorov.paistewiki.Adapter.CustomAdapter;
-import ru.alexsuvorov.paistewiki.Adapter.RecyclerAdapter;
+import ru.alexsuvorov.paistewiki.Adapter.NewsAdapter;
 import ru.alexsuvorov.paistewiki.R;
-import ru.alexsuvorov.paistewiki.Utils.NewsLoader;
 import ru.alexsuvorov.paistewiki.model.NewsMonth;
 
 public class NewsFragment extends Fragment {
 
-    private static final int LOAD_MORE_MONTH = 20;
-    private String urlMain = "http://paiste.com/e/news.php?menuid=39";
     private static final String TAG = "NewsFragment";
-    //private String urlMain = "http://paiste.com/e/news.php?menuid=39&actn=monthlist&type=all&year=2017&month=10";
-    private String linksText;
-    private String linkNews;
-    private Elements title;
-    private TableLayout tableRow;
-    private TextView Title;
     private CustomAdapter VPadapter;
-    private NewsLoader parser;
 
     public static ArrayList<NewsMonth> newsArray = new ArrayList<NewsMonth>();
-    //ArrayList<NewsModel> newsArray = (ArrayList<NewsModel>)getArguments().getSerializable("newsArray");
-    private ListView newsList;
-    RecyclerView recyclerView;
-    private RecyclerAdapter adapter;
-
-    // The minimum amount of items to have below your current scroll position
-    // before loading more.
-    private int visibleThreshold = 5;
-    private int lastVisibleItem, totalItemCount;
-    private boolean loading;
-
-    //List<NewsModel> listArray = parser.getNews();
-    //private ProgressDialog progressdialog;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.news_fragment, container, false);
-        tableRow = view.findViewById(R.id.tableNews);
-        Title = view.findViewById(R.id.labelNews);
 
         //newsList = view.findViewById(R.id.newsList);
         /*savedInstanceState = this.getArguments();
@@ -82,19 +53,16 @@ public class NewsFragment extends Fragment {
         /*NewsAdapter newsAdapter = new NewsAdapter(this.getContext(),
                 R.layout.news_item, newsArray);
         newsList.setAdapter(newsAdapter);*/
-        adapter = new RecyclerAdapter();
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.newsList);
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(llm);
+
+        NewsAdapter adapter = new NewsAdapter(month);
+        recyclerView.setAdapter(adapter);
         //adapter.setData(newsList);
 
         //recyclerView.setAdapter(adapter);
 
         return view;
     }
-
-
-/*
-    void setItems(List<NewsModel> items) {
-        if (adapter != null) {
-            adapter.setData(items);
-        }
-    }*/
 }
