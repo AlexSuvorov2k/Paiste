d9a21e732ed7d3e2f506eb129ed1450a273cbd9b
AlexS <alexsuvorov94@mail.ru>
2018-07-20 17:17:43 +0400

    -c

--- app/src/main/java/ru/alexsuvorov/paistewiki/Fragment/NewsFragment.java
+++ app/src/main/java/ru/alexsuvorov/paistewiki/Fragment/NewsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -20,12 +21,12 @@ import ru.alexsuvorov.paistewiki.model.NewsMonth;
 
 public class NewsFragment extends Fragment {
 
-    private static final String TAG = "NewsFragment";
-    private ImageSliderAdapter sliderAdapter;
     private List<NewsMonth> monthArray;
+    private RecyclerView recyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.news_fragment, container, false);
     }
 
@@ -34,16 +35,17 @@ public class NewsFragment extends Fragment {
                               Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ViewPager viewPager = view.findViewById(R.id.view_pager);
-        sliderAdapter = new ImageSliderAdapter(getContext());
+        ImageSliderAdapter sliderAdapter = new ImageSliderAdapter(getContext());
         viewPager.setAdapter(sliderAdapter);
 
         initializeData();
         RecyclerView recyclerView = view.findViewById(R.id.newsList);
-        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
-        recyclerView.setLayoutManager(llm);
+        //LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
 
         NewsAdapter adapter = new NewsAdapter(monthArray); /*this.getContext(), R.layout.news_item, monthArray*/
         recyclerView.setAdapter(adapter);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
     }
 
     private void initializeData() {
