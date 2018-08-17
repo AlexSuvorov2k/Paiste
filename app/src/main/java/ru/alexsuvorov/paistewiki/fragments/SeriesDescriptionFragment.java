289907692911c8bfe11b45c0f51345f980e65462
alexsuvorov94@mail.ru <A1A1Ca15>
2018-08-17 13:06:06 +0400

    Database loaded

--- app/src/main/java/ru/alexsuvorov/paistewiki/fragments/SeriesDescriptionFragment.java
+++ app/src/main/java/ru/alexsuvorov/paistewiki/fragments/SeriesDescriptionFragment.java
@@ -2,6 +2,7 @@ package ru.alexsuvorov.paistewiki.fragments;
 
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.text.Html;
 import android.view.LayoutInflater;
@@ -9,66 +10,33 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import ru.alexsuvorov.paistewiki.R;
 
 public class SeriesDescriptionFragment extends Fragment {
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.cymbal_description, container, false);
-
         Bundle bundle = getArguments();
-        Integer selectedProduct;
-
-        //ReDesing link:  https://habrahabr.ru/post/270121/
-
         if (bundle != null) {
-            selectedProduct = bundle.getInt("productIndex");
-            Toast toast = Toast.makeText(getContext(),
-                    "Выбран продукт номер " + selectedProduct, Toast.LENGTH_SHORT);
-            //toast.show();
-
-
-            //Series Image
+            int position = bundle.getInt("cymbalseries_id");
 
             TypedArray cymbalSeriesPicsArr = getResources().obtainTypedArray(R.array.cymbalSeriesPics);
             ImageView img = view.findViewById(R.id.seriesLogo);
-            img.setImageDrawable(cymbalSeriesPicsArr.getDrawable(selectedProduct));
+            img.setImageDrawable(cymbalSeriesPicsArr.getDrawable(position));
 
             cymbalSeriesPicsArr.recycle();
 
-
             //Series Name
             String[] cymbalSeriesNamesArr = getResources().getStringArray(R.array.cymbalSeriesNames);
-            getActivity().setTitle(cymbalSeriesNamesArr[selectedProduct]);
-            /*
-            TextView seriesName = (TextView) view.findViewById(R.id.cymbalSeriesName);
-            seriesName.setText(cymbalSeriesNamesArr[selectedProduct]);
-
-
-        <TextView
-            android:id="@+id/cymbalSeriesName"
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:text=""
-            android:textSize="20sp"
-            android:textColor="@color/black"
-            android:textStyle="bold"
-            android:layout_marginTop="17dp"
-            android:layout_below="@+id/seriesLogo"
-            android:layout_alignParentLeft="true"
-            android:layout_alignParentStart="true" />
-            */
+            getActivity().setTitle(cymbalSeriesNamesArr[position]);
 
             //Описание серии
             String[] cymbalSeriesDescriptionArr = getResources().getStringArray(R.array.cymbalSeriesDescription);
             TextView seriesDescription = view.findViewById(R.id.seriesDescriptionText);
-            seriesDescription.setText(Html.fromHtml(cymbalSeriesDescriptionArr[selectedProduct]));
-
+            seriesDescription.setText(Html.fromHtml(cymbalSeriesDescriptionArr[position]));
         }
-
         return view;
     }
 
